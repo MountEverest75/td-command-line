0e39dde668b21de275fbb921c99c9741767f815a
MountEverest75 <ravikiranmailid@gmail.com>
2016-06-14 12:26:09 -0700

    Refined command line utility

--- lib/query.py
+++ lib/query.py
@@ -1,5 +1,4 @@
 #!/Users/rchebolu/anaconda3/anaconda/bin/python3
-
 import sys, getopt
 import os
 import pandas as pd
@@ -15,11 +14,14 @@ def run_dynamic_query(parameters):
     con = td.connect(apikey, endpoint)
     #1. Connect to the query engine
     con_engine=con.query_engine(database=parameters['db_name'], type=parameters['query_engine'])
+
+	#2. Setup query limit string
     if parameters['limit'] != '0':
         limit_str = "LIMIT " + str(parameters['limit']) + ";"
     else:
         limit_str = ";"
     
+	#3. Compose Query String
     if parameters['min_time'] == 'NULL' and parameters['max_time'] == 'NULL':
         compose_query = "SELECT " + parameters['col_list']   + " " + \
                         "FROM "   + parameters['table_name'] + " " + limit_str
@@ -30,14 +32,15 @@ def run_dynamic_query(parameters):
                         "WHERE "  + "td_time_range(time,"    + parameters['min_time'] + "," + parameters['max_time'] + ") " + \
                         limit_str
             
-    print(compose_query)
-    #2. Run query as a job and wait for job to finish
+    print("Executing..." + compose_query)
+    #4. Run query as a job and wait for job to finish
     with tdclient.Client(apikey) as client:
         job = client.query(parameters['db_name'],compose_query,type=parameters['query_engine'])
         job.wait()
+		#Assign result set to a data frame
         df = td.read_td_job(job.job_id, con_engine)
 
-    #3. Write the results to a csv file
+    #5. Write the results to a csv or tabular format file
     if parameters['format'] == 'csv':
         print("Downloading results to " + job.job_id + ".csv" + " file")
         df.to_csv(job.job_id + ".csv")
@@ -49,7 +52,24 @@ def run_dynamic_query(parameters):
         outfile.write(tabulate(df, tablefmt="grid"))
         outfile.close()
 
+#Function to check if a value if numeric
+def is_number(s):
+	try:
+		float(s) # for int, long and float
+	except ValueError:
+		return False
+	return True
+
+#Main function receives command line arguments
 def main(argv):
+	db_name    = None
+	table_name = None
+	format     = None
+	engine     = None
+	columns    = None
+	min_time   = None
+	max_time   = None
+	limit      = None
 	try:
 		opts, args = getopt.getopt(argv,"f:e:c:m:M:l:d:t:",["format=","engine=","columns=","min_time=","max_time=","limit=","db_name=","table_name="])
 	except getopt.GetoptError:
@@ -77,36 +97,58 @@ def main(argv):
 			table_name = arg
 			
 	parameters = {}
+	if db_name == None and table_name == None:
+		print("Database Name and Table Name are mandatory")
+		print("Suggested usage: ./query.py -d <db_name> -t <table_name>")
+		print("Suggested usagewith optionals: query.py [-f <format(tabular or csv)> -e <engine()> -c <columns separated by commas> -m <min_time> -M <max_time> -l <limit>] -d db_name -t table_name")
+		return
+		
 	parameters['db_name'] = db_name
 	parameters['table_name'] = table_name
-	if engine == '':
+	if engine == None or engine.strip() == '':
 		parameters['query_engine'] = "presto"
 	else:
-		parameters['query_engine'] = engine
+		parameters['query_engine'] = engine.strip()
 		
-	if columns == '':
+	if columns == None or columns.strip() == '':
 		parameters['col_list'] = "*"
 	else:
-		parameters['col_list'] = columns
+		parameters['col_list'] = columns.strip()
 		
-	if format == 'csv':
-		parameters['format'] = 'csv'
-	else:
+	if  format == None or format.strip() == '':
 		parameters['format'] = 'tabular'
+	else:
+		if format.strip() in ["tabular", "csv"]:
+			parameters['format'] = format.strip()
+		else:
+			print("Invalid format option specified. Valid options are tabular or csv")
+			return
 		
-	# if limit == '':
-	# 	parameters['limit'] = str(limit)
-	# else:
-	# 	parameters['limit'] = '0'
-	# if min_time == '':
-	# 	parameters['min_time'] = 'NULL'
-	# 
-	# if max_time == '':
-	# 	parameters['max_time'] = 'NULL'
-	parameters['limit'] = str(limit)
-	parameters['min_time'] = min_time
-	parameters['max_time'] = max_time
+	if limit == None or limit.strip() == '':
+		parameters['limit'] = str('0')
+	else:
+		if is_number(limit):
+			parameters['limit'] = str(limit).strip()
+		else:
+			print("Non numeric value specified for query limit")
+			print("./query.py [-f <format(tabular or csv)> -e <engine()> -c <columns separated by commas> -m <min_time> -M <max_time> -l <limit>] -d db_name -t table_name")
+			return
+		
+	if min_time == None or min_time.strip() == '':
+		parameters['min_time'] = 'NULL'
+	else:
+		if is_number(min_time):
+			parameters['min_time'] = min_time
+		else:
+			print("Non numeric value specified for minimum timestamp. Vaid example is 1412366345")
 	
+	if max_time == None or max_time.strip() == '':
+		parameters['max_time'] = 'NULL'
+	else:
+		if is_number(max_time):
+			parameters['max_time'] = max_time
+		else:
+			print("Non numeric value specified for maximum timestamp. Vaid example is 1412366345")
 	print(parameters)
 	run_dynamic_query(parameters)
 
--- lib/query_test.sh
+++ lib/query_test.sh
@@ -1,2 +1,6 @@
-./query.py -f 'csv' -e 'presto' -c '*' -m 'NULL' -M 'NULL' -l '100' -d 'sample_datasets' -t 'www_access'
-./query.py -f 'csv' -e 'presto' -c '*' -m '1412366345' -M '1412366395' -l '100' -d 'sample_datasets' -t 'www_access'
+./query.py -f 'csv' -e 'presto' -c '*' -m 'NULL' -M 'NULL' -l '100' -d 'sample_datasets' -t 'www_access' >> tdaudit.log
+./query.py -f 'csv' -e 'presto' -c '*' -m '1412366345' -M '1412366395' -l '100' -d 'sample_datasets' -t 'www_access' >> tdaudit.log
+./query.py -f 'csv' -e 'presto' -c '*' -m 'NULL' -M '1412366395' -l '100' -d 'sample_datasets' -t 'www_access' >> tdaudit.log
+./query.py -f 'csv' -e 'presto' -c '*' -m '1412366345' -M 'NULL' -l '100' -d 'sample_datasets' -t 'www_access' >> tdaudit.log
+./query.py -f 'csv' -e 'presto' -c '*' -m '1412366345' -M 'NULL' -l '100' -d 'sample_datasets' -t 'www_access' >> tdaudit.log
+./query.py -d 'sample_datasets' -t 'www_access' >> tdaudit.log
